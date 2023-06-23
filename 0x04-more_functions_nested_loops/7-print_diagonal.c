d5db45662e8a140e9d197cc1264818f8030efcc6
danah57 <danahsafwat@icloud.com>
2023-06-22 19:24:59 -0700

    a

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -5,22 +5,22 @@
  *
  * @n: is the number of times the \ character
  *   should be printed
+ *   Return: void
 */
 
 void print_diagonal(int n)
 {
-	int postn, space;
+	int i, j;
 
 	if (n <= 0)
 		_putchar('\n');
-	else
+	for (i = 0; j < n; i++)
 	{
-		for (postn = 1; postn <= n; postn++)
+		for (j = 0; j < i; j++)
 		{
-			for (space = 1; space <= postn; space++)
-				_putchar(' ');
-			_putchar(92);/*is equal to '/'char*/
-			_putchar('\n');
+			_putchar(' ');
 		}
+		_putchar('\\');
+		_putchar('\n');
 	}
 }
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,7 +3,7 @@
 
 #include <stdio.h>
 
-int _putchar(char);
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -12,5 +12,6 @@ void print_most_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
+void print_triangle(int size);
 
 #endif
