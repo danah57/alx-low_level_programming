484d4f01ebad5cc5c2d41d335df6f6fe3bb5fcd0
danah57 <danahsafwat@icloud.com>
2023-06-29 18:29:09 -0700

    a

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -10,7 +10,7 @@ char *rot13(char *s)
 {
 	int i;
 
-	char rot13[] = "abcdefghijkmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char rot13[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	char ROT13[] = "nopqrstuvwxyzabcdefghijkimNOPOQRSTUVWXYZABCDEFGHIJKLM";
 	char *ptr = s;
 
