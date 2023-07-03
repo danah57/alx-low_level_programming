2dad0bffb57525c384ee5a289925c0c553057274
danah57 <danahsafwat@icloud.com>
2023-07-03 14:50:37 -0700

    a

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_chessboard - print chessboard
+ *
+ * @a: rows
+ * Return: nothing
+*/
+
+void print_chessboard(char (*a)[8])
+{
+	int itr, jtr;
+
+	for (itr = 0; itr < 0; itr++)
+	{
+		for (jtr = 0; jtr < 0; jtr++)
+		{
+			_putchar (a[itr][jtr]);
+		}
+		_putchar('\n');
+	}
+}
