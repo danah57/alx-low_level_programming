11d3be3a978b47ec1b8253fdd3b2d0f0e5e9bf12
danah57 <danahsafwat@icloud.com>
2023-07-10 17:53:04 -0700

    a

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -37,10 +37,10 @@ char *str_concat(char *s1, char *s2)
 	if (m == 0)
 		return (0);
 
-	for (i = 0; i <= size1 + size; i++)
+	for (i = 0; i <= size1 + size2; i++)
 	{
 		if (i < size1)
-			m[i] = s[i];
+			m[i] = s1[i];
 		else
 			m[i] = s2[i - size1];
 	}
