d403153fe858b63398920bef3f76d54df0867e0a
danah57 <danahsafwat@icloud.com>
2023-07-13 16:06:42 -0700

    a

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -10,31 +10,38 @@
 */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *p;
+	char *nptr;
 	unsigned int i;
 
 	if (new_size == old_size)
 		return (ptr);
-	if (new_size == 0 && ptr != NULL)
-	{
-		free(ptr);
-		return (NULL);
-	}
+	
 	if (ptr == NULL)
 	{
-		p = malloc(new_size);
-		if (p == NULL)
+		nptr = malloc(new_size);
+
+		if (nptr == NULL)
 			return (NULL);
 		return (p);
 	}
-	if (new_size > old_size)
+	else
 	{
-		p = malloc(new_size);
-			if (p == NULL)
-				return (NULL);
-		for (i = 0; i < old_size && i < new_size; i++)
-			*((char *)p + i) = *((char *)ptr + i);
-		free(ptr);
+		if (new_size == 0)
+		{
+			free(ptr);
+			return (NULL);
+		}
 	}
-	return (0);
+	nptr = malloc(new_size);
+
+	if (nptr == NULL)
+		return (NULL);
+
+	for (i = 0; i < old_size && i < new_size; i++)
+	{
+		nptr[i] = ((char *) ptr)[i];
+	}
+
+	free(ptr);
+	return (nptr);
 }
