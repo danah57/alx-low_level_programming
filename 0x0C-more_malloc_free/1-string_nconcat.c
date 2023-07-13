a2f1fbdf5adeb329f53788b59c97b8ea78365873
danah57 <danahsafwat@icloud.com>
2023-07-13 15:13:34 -0700

    a

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,9 +13,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i, j, s1_length, s2_length;
 
 	if (s1 == NULL)
-		s1 = "";
+		s1 = " ";
 	if (s2 == NULL)
-		s2 = "";
+		s2 = " ";
 
 	for (s1_length = 0; s1[s1_length] != '\0'; s1_length++)
 		;
