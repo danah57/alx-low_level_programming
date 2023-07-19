779512fb6ad1fd9e336060cd55cc1d2da81f2672
danah57 <danahsafwat@icloud.com>
2023-07-18 18:47:20 -0700

    a

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -5,7 +5,7 @@
  * struct dog - a dog struct
  * @name: dog's name
  * @age: dog's age
- * owner: owner's name
+ * @owner: owner's name
  *
  * Description: just a lone dog struct in a big litty world
 */
@@ -15,5 +15,9 @@ struct dog
 	float age;
 	char *owner;
 };
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
